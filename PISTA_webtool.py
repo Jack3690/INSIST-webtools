7a2dd53f014d1abdb04b205a9215caa21b255ddc
Avinash CK <44706894+Jack3690@users.noreply.github.com>
2022-08-30 12:22:55 +0530

    Update PISTA_webtool.py

--- PISTA_webtool.py
+++ PISTA_webtool.py
@@ -19,7 +19,7 @@ def _max_width_():
         unsafe_allow_html=True,
     )
 _max_width_()
-c30, c31, c32 = st.columns([80, 1, 3])
+c30, c31, c32 = st.columns([200, 1, 3])
 
 with c30:
     st.title("Python Image Simulation and Testing Application")
