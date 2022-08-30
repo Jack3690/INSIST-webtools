2a1fea1a97e8bc8dbf57f7260267e4d9de6847d6
Avinash CK <44706894+Jack3690@users.noreply.github.com>
2022-08-30 12:26:09 +0530

    Update PISTA_webtool.py

--- PISTA_webtool.py
+++ PISTA_webtool.py
@@ -6,18 +6,6 @@ st.set_page_config(
     page_title="PISTA",
 )
 
-def _max_width_():
-    max_width_str = f"max-width: 2800px;"
-    st.markdown(
-        f"""
-    <style>
-    .reportview-container .main .block-container{{
-        {max_width_str}
-    }}
-    </style>    
-    """,
-        unsafe_allow_html=True,
-    )
-_max_width_()
+
 st.title("PISTA")
 st.header("Python Image Simulation and Testing Application")
