df1fde98b15e2c20f38f839f930f61f641f7a3bd
Avinash CK <44706894+Jack3690@users.noreply.github.com>
2022-08-30 16:12:40 +0530

    Update PISTA_webtool.py

--- PISTA_webtool.py
+++ PISTA_webtool.py
@@ -51,6 +51,7 @@ with st.form(key="my_form"):
 
 
 if df_upload is not None:
+	st.write(df_upload.type)
 	if 'csv' in df_upload.type:
 		df = pd.read_csv(df_upload)
 	if 'fit' in df_upload.type or 'fits' in df_upload.type :
