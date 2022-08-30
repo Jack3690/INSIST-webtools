5697fa500deadf2b55997ce7043918f6ad257050
Avinash CK <44706894+Jack3690@users.noreply.github.com>
2022-08-30 14:46:43 +0530

    Update PISTA_webtool.py

--- PISTA_webtool.py
+++ PISTA_webtool.py
@@ -44,6 +44,11 @@ if df_upload is not None:
 		df = pd.read_csv(df_upload)
 	if 'fit' in df_upload.type or 'fits' in df_upload.type :
 		df = Table.read(df_upload).to_pandas()
+else:
+	ra = [0]
+	dec = [0]
+	mag = [10]
+	df = pd.DataFrame(zip(ra,dec,mag), columns = ['ra','dec','manga'])
 
 st.write(df_upload)
 if submit_button:
