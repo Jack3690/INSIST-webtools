19d4d2fda10b294b693d74ad2f85449801c6ad62
Avinash CK <44706894+Jack3690@users.noreply.github.com>
2022-08-30 12:16:52 +0530

    Update PISTA_webtool.py

--- PISTA_webtool.py
+++ PISTA_webtool.py
@@ -2,4 +2,21 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Python Image Simulation and Testing Application (PISTA)')
+st.set_page_config(
+    page_title="Python Image Simulation and Testing Application (PISTA)",
+)
+
+def _max_width_():
+    max_width_str = f"max-width: 1400px;"
+    st.markdown(
+        f"""
+    <style>
+    .reportview-container .main .block-container{{
+        {max_width_str}
+    }}
+    </style>    
+    """,
+        unsafe_allow_html=True,
+    )
+_max_width_()
+c30, c31, c32 = st.columns([2.5, 1, 3])
