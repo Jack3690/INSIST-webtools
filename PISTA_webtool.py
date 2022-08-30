43b33dfc79d4c92a44b4379ea30973eaffcf47dc
Avinash CK <44706894+Jack3690@users.noreply.github.com>
2022-08-30 12:28:04 +0530

    Update PISTA_webtool.py

--- PISTA_webtool.py
+++ PISTA_webtool.py
@@ -9,3 +9,15 @@ st.set_page_config(
 
 st.title("PISTA")
 st.header("Python Image Simulation and Testing Application")
+
+with st.expander("ℹ️ - About this app", expanded=True):
+
+    st.write(
+        """     
+-   PISTA simulates individual stars and adds different noises. 
+    The input parameter space is designed to inculcate observational parameters,
+    telescope parameters and detector characteristics.
+	    """
+    )
+
+    st.markdown("")
