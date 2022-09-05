a8940e4632b328f01df13e72015441adbbc2c409
Avinash CK <44706894+Jack3690@users.noreply.github.com>
2022-09-05 16:22:02 +0530

    Update PISTA_webtool.py

--- PISTA_webtool.py
+++ PISTA_webtool.py
@@ -58,7 +58,6 @@ if df_upload is not None:
 	if 'fit' in df_upload.type or 'fits' in df_upload.type or 'octet' in df_upload.type:
 		hdul = fits.open(df_upload)
 		for hdu in hdul:
-			st.write(hdu.header)
 			if 'XTENSION' in hdu.header.keys():
 				if hdu.header['XTENSION']=='BINTABLE':
 					tab = Table(hdu.data)
