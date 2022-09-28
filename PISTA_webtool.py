1b57478fb9e7033250c6ca2635f29bcc71b04da1
Avinash CK <44706894+Jack3690@users.noreply.github.com>
2022-09-28 11:03:26 +0530

    Update PISTA_webtool.py

--- PISTA_webtool.py
+++ PISTA_webtool.py
@@ -80,8 +80,8 @@ if submit_button:
 	sim = pis.Analyzer(df=df, exp_time = exp_time, n_x = n_x, n_y = n_y)
 	sim()
 	st.write(sim.digital.shape)
-	fig, ax = plt.subplots()
-	ax.imshow(sim.digital)
+	fig, ax = sim.show_field()
+	st.write(type(fig))
 	with c2:
 		
 		img = st.pyplot(fig=fig)
