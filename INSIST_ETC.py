95d9bcf0bfa810f1f268fd59511eed9ee4aaa945
Avinash CK <44706894+Jack3690@users.noreply.github.com>
2023-11-11 17:00:15 -0600

    Update INSIST_ETC.py

--- INSIST_ETC.py
+++ INSIST_ETC.py
@@ -56,7 +56,7 @@ def exposure_time(det_params,M,SNR):
 
   A =  (F_m_p/SNR)**2
   B = -(F_m_p + F_sky_p*n_pix + dark_current*n_pix)
-  C = -n_pix*(read_noise)**2
+  C = -(n_pix*read_noise)**2
 
   t1 = (-B + np.sqrt(B**2 - 4*A*C))/(2*A)
   t2 = (-B - np.sqrt(B**2 - 4*A*C))/(2*A)
