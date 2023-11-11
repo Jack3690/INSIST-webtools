5b141da99dfa3fbb96f07b3fb3955c9d942e7370
Avinash CK <44706894+Jack3690@users.noreply.github.com>
2023-11-11 16:45:02 -0600

    Update INSIST_ETC.py

--- INSIST_ETC.py
+++ INSIST_ETC.py
@@ -165,7 +165,7 @@ if submit_button:
 
 	exp_time = exposure_time(det_params,mag,SNR)
 	sim = pt.Imager(df, tel_params=tel_params, n_x=100, n_y=100, exp_time=exp_time)
-	sim(det_params=det_params, photometry = None)
+	#sim(det_params=det_params, photometry = None)
 	with c2:
 		fig, ax = sim.show_image()
 		st.pyplot(fig)
